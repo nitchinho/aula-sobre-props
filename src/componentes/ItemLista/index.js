cab2aeaaa5219a929871a2158c963508345fe7e0
Nitchinho <nitchinho007@gmail.com>
2023-08-21 11:43:03 -0300

    finalizado exemplo

--- src/componentes/ItemLista/index.js
+++ src/componentes/ItemLista/index.js
@@ -3,15 +3,15 @@ import './estilo.css';
 
 export default function ItemLista(props) {
   return (
-    <div className="boxSigno">
-      <div className="nomeSigno">
-        {props.signo}
+    <div className = "boxSigno">
+      <div className = "nomeSigno">
+        { props.signo }
       </div>
-      
-      <img className="imagemSigno" src={props.imagem} alt={props.signo}/>
-      
+
+      <img className = "imagemSigno" src = { props.imagem } alt = { props.signo } />
+
       <div className="periodoNascimento">
-        {props.dataInicio} - {props.dataFim}
+        { props.dataInicio } - { props.dataFim }
       </div>
     </div>
   )
